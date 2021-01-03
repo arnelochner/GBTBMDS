bb40bf3e85a6280add50a07e326db67be2d0d9e3
Arne Lochner <arnemartin.lochner@gmail.com>
2021-01-03 13:03:23 +0100

    adding perl dependecies for installer!

--- installer.sh
+++ installer.sh
@@ -10,4 +10,11 @@ conda install -y -c conda-forge sentencepiece
 conda install -y -c anaconda nltk
 conda install -y -c paddle paddlepaddle-gpu="1.8.1 py36_gpu_cuda10.0_many_linux"
 conda install -y -c nvidia nccl
+conda install -c bioconda perl-xml-libxml 
+conda install -c bioconda perl-lwp-protocol-https
+
+cd pyrogue/tools/ROUGE-1.5.5/data
+rm WordNet-2.0.exc.db
+./WordNet-2.0-Exceptions/buildExeptionDB.pl ./WordNet-2.0-Exceptions ./smart_common_words.txt ./WordNet-2.0.exc.db
+
 
