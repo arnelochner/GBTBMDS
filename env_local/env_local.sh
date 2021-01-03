56cf96c5a38b8888b79de729acc0fcaba2f9db60
Arne Lochner <arnemartin.lochner@gmail.com>
2021-01-03 19:30:43 +0100

    perl env variable added

--- env_local/env_local.sh
+++ env_local/env_local.sh
@@ -7,3 +7,16 @@ export iplist=`hostname -i` # ip of the local machine
 #http_proxy
 unset http_proxy
 unset https_proxy
+
+xml_parser=$(perldoc -l XML::Parser)
+xml_parser_parent=$(dirname $xml_parser)
+xml_parser_grandparent=$(dirname $xml_parser_parent)
+
+
+lwp_user=$(perldoc -l LWP::UserAgent)
+lwp_user_parent=$(dirname $lwp_user)
+lwp_user_grandparent=$(dirname $lwp_user_parent)
+
+export PERL5LIB=$xml_parser_grandparent:$lwp_user_grandparent
+
+echo $PERL5LIB
