7a0db75f465a248e68b82c027286b8facdcd9c05
Arne Lochner <arnemartin.lochner@gmail.com>
2021-01-07 16:16:31 +0100

    preprocessing

--- src/data_preprocess/graphsum/data_builder_graphsum.py
+++ src/data_preprocess/graphsum/data_builder_graphsum.py
@@ -57,7 +57,15 @@ def format_to_json(args):
                      'valid': [args.valid_src, args.valid_tgt],
                      'test': [args.test_src, args.test_tgt]}
 
-    for corpus_type in ['test', "train"]:
+    datasets = ["test", "train", "valid"]
+
+    if args.dataset != "":
+        datasets = [args.dataset]
+
+    print(args.dataset)
+    print(datasets)
+
+    for corpus_type in datasets:
         src_files = codecs.open(
             corpora_files[corpus_type][0], 'r', 'utf-8').readlines()[:args.num_examples]
         tgt_files = codecs.open(
@@ -127,8 +135,7 @@ def format_to_paddle(args):
     for corpus_type in datasets:
         a_lst = []
         for json_f in glob.glob(pjoin(args.json_path, '*' + corpus_type + '.*.json')):
-            real_name = json_f.split('\\')[-1]
-            print(real_name)
+            real_name = json_f.split('/')[-1]
             a_lst.append((json_f, args, pjoin(args.data_path,
                                               "MultiNews." +
                                               str(args.max_nsents)
@@ -151,6 +158,7 @@ def _format_to_paddle(params):
 
     summ_data = SummData(args)
 
+    print("Save File %s" % save_file)
     print('Processing %s' % json_file)
     jobs = json.load(open(json_file))
 
